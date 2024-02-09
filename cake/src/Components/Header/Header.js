62afffad3528bdebb6ca3ef6499addeba49cc3b3
Shamindi <ngdilshan2002@gmail.com>
2024-02-09 14:02:28 +0530

    nav bar css

--- cake/src/Components/Header/Header.js
+++ cake/src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import { FaHeart } from "react-icons/fa";
 import { FaBagShopping } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa";
 import Logo from "./img/logo.png";
 function Header() {
   return (
@@ -11,14 +12,24 @@ function Header() {
           <img src={Logo} alt="logo" className="cakelogo" />
         </div>
         <div className="nav-names">
-          <a href="/" className="nav-item" >Home</a>
-          <a href="/" className="nav-item">Cake</a>
-          <a href="/" className="nav-item">About</a>
-          <a href="/" className="nav-item">Contact</a>
+          <a href="/" className="nav-item">
+            Home
+          </a>
+          <a href="/" className="nav-item">
+            Cake
+          </a>
+          <a href="/" className="nav-item">
+            About
+          </a>
+          <a href="/" className="nav-item">
+            Contact
+          </a>
         </div>
         <div className="cart-icon">
-          <a ><FaHeart /></a>
-          <FaBagShopping />
+      
+         <span className="iconmain"> <FaHeart className="left-icon" /></span>
+         <span className="iconmain"> <FaBagShopping className="left-icon" /></span>
+         <span className="iconmain"><FaUser className="left-icon" /></span>
         </div>
       </div>
     </div>
