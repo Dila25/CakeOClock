fe17fc8698a089ed21d2707c6fe2ff539c8c64a0
Shamindi <ngdilshan2002@gmail.com>
2024-02-09 13:05:25 +0530

    Update bk

--- cake/src/App.js
+++ cake/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 function App() {
   return (
     <div>
+      <Header/>
       <React.Fragment>
         <Routes>
           <Route path="/" element={<Home />} />
--- cake/src/Components/Header/Header.js
+++ cake/src/Components/Header/Header.js
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import "./header.css";
+import Logo from "./img/logo.png";
 function Header() {
   return (
     <div>
-      
+      <div className="navbar">
+        <div className="logo">
+          <img src={Logo} alt="logo" className="cakelogo" />
+        </div>
+        <div className="nav-names">
+          <a href="/" className="nav-item" >Home</a>
+          <a href="/" className="nav-item">Cake</a>
+          <a href="/" className="nav-item">About</a>
+          <a href="/" className="nav-item">Contact</a>
+        </div>
+        <div className="cart-icon">Cart Icon</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
