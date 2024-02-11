56ae25d8558f9c098046700dea40e3b2e5aa1537
Shamindi1211 <it21203558@my.sliit.lk>
2024-02-11 08:32:12 +0530

    Login Done

--- cake/src/Components/Login/Login.js
+++ cake/src/Components/Login/Login.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Header from "../Header/Header";
+import Logo from "../Header/img/logo.png";
 import "./Login.css";
 
 function Login() {
   return (
     <div>
+     
       <div className="background">
-        <Header />
+        <Header/>
+    <div className="login-container">
+  
         
+        <div className="login-box">
+          <img src={Logo} alt="Logo" className="login-logo" />
+          <h2 className="login-heading">Log in</h2>
+          <form className="login-form">
+            <div className="login-form-group">
+              <label htmlFor="email" className="login-label">USER NAME/ EMAIL</label>
+              <input type="email" id="email" placeholder="Email" className="login-form-input" />
+            </div>
+            <div className="login-form-group">
+              <label htmlFor="password" className="login-label">PASSWORD</label>
+              <input type="password" id="password" placeholder="Password" className="login-form-input" />
+            </div>
+            <a href="#" className="login-forgot-password">Forgot Your Password?</a>
+            <div className='login-btn'>
+            <button type="submit" className="login-button">Login</button>
+            </div>
+          </form>
+        </div>
       </div>
+    </div>
     </div>
   );
 }
